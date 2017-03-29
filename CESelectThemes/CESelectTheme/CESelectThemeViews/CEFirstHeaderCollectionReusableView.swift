f6fb2e9edc0209642abd4c9d8b5c0c3a71522778
zeluliCredit <zeluli@creditease.cn>
2017-03-29 09:28:32 +0800

    code review

--- CESelectThemes/CESelectTheme/CESelectThemeViews/CEFirstHeaderCollectionReusableView.swift
+++ CESelectThemes/CESelectTheme/CESelectThemeViews/CEFirstHeaderCollectionReusableView.swift
@@ -10,15 +10,15 @@ import UIKit
 typealias TapEditButtonClosure = (Bool) -> Void
 class CEFirstHeaderCollectionReusableView: CEHeaderCollectionReusableView {
     
-    var detailLabel: UILabel!
     var editButton: CEThemeEditButton!
     var editButtonClosure: TapEditButtonClosure!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = UIColor.clear
-        self.addDetailLabel()
         self.addEditButton()
+        self.detailLabel.text = "拖动可以排序"
+        self.detailLabel.isHidden = true
     }
     
     func setTapEditButtonClosure(editButtonClosure: @escaping TapEditButtonClosure) {
@@ -33,16 +33,6 @@ class CEFirstHeaderCollectionReusableView: CEHeaderCollectionReusableView {
         self.editButton = CEThemeEditButton(frame: CGRect(x: self.frame.width - 55, y: 30, width: 40, height: 25))
         self.editButton.addTarget(self, action: #selector(tapEditButton(sender:)), for: .touchUpInside)
         self.addSubview(self.editButton)
-    }
-    
-    func addDetailLabel() {
-        self.detailLabel = UILabel(frame: CGRect(x: self.titleLabel.frame.width, y: 28, width: 100, height: 20))
-        self.detailLabel.font = UIFont.systemFont(ofSize: 11)
-        self.detailLabel.textColor = UIColor.lightGray
-        self.detailLabel.text = "拖动可以排序"
-        self.detailLabel.isHidden = true
-        self.addSubview(detailLabel)
-
     }
     
     func tapEditButton(sender: CEThemeEditButton) {
--- CESelectThemes/CESelectTheme/CESelectThemeViews/CEHeaderCollectionReusableView.swift
+++ CESelectThemes/CESelectTheme/CESelectThemeViews/CEHeaderCollectionReusableView.swift
@@ -10,15 +10,26 @@ import UIKit
 
 class CEHeaderCollectionReusableView: UICollectionReusableView {
     var titleLabel: UILabel!
+    var detailLabel: UILabel!
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.addTitleLabel()
+        self.addDetailLabel()
     }
     
     func addTitleLabel() {
         self.titleLabel = UILabel(frame: CGRect(x: 15, y: 30, width: 85, height: 20))
         self.titleLabel.font = UIFont.boldSystemFont(ofSize: 15)
         self.addSubview(titleLabel)
+    }
+    
+    func addDetailLabel() {
+        self.detailLabel = UILabel(frame: CGRect(x: self.titleLabel.frame.width, y: 30, width: 100, height: 20))
+        self.detailLabel.font = UIFont.systemFont(ofSize: 11)
+        self.detailLabel.textColor = UIColor.lightGray
+        self.detailLabel.text = "点击可以添加"
+        self.addSubview(detailLabel)
+        
     }
 
     required init?(coder aDecoder: NSCoder) {
